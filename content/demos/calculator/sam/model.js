49ac68c4fba4826c0998b522b91932bd19ca864f
david kaye <dfkaye@yahoo.com>
2021-01-12 17:13:59 -0800

    calculator, start filling in the model steps

--- content/demos/calculator/sam/model.js
+++ content/demos/calculator/sam/model.js
@@ -2,24 +2,183 @@
 export { model }
 
 function model(state) {
-  var data = {
-
+  var base = {
+    input: [],
+    output: "0"
   }
 
-  function merge(data) {
-    return data
+  var data = {}
+
+  function change({ data, changes }) {
+
+    // Merge changes directly into data.
+    return Object.assign(data, changes);
   }
 
   var steps = {
+    backspace() {
+      var { output } = data
 
+      var changes = {
+        output: output.substring(0, output.length - 1)
+      }
+
+      return change({ data, changes })
+    },
+
+    clear() {
+      var changes = Object.assign({}, base)
+
+      return change({ data, changes })
+    },
+
+    clearentry() {
+      var changes = { output: base.output }
+
+      return change({ data, changes })
+    },
+
+    decimal() {
+      var { output } = data
+
+      if (output.indexOf(".") != -1) {
+        // Ignore repeated decimal input.
+        return
+      }
+
+      var changes = {
+        output: output + "."
+      }
+
+      return change({ data, changes })
+    },
+
+    digit({ value }) {
+      if (/\D/.test(value)) {
+        // Notify state of errors
+        return state.transition({
+          error: `invalid digit value, "${value}"`
+        })
+      }
+
+      var { input } = data
+      var last = input[input.length - 1]
+
+      // If last action was equals(), reinitialize to clean state.
+      if (last == "equals") {
+        steps.clear()
+      }
+
+      var input = data.input.slice()
+
+      input.push(value)
+
+      var { output } = data
+
+      output = output == 0
+        ? value
+        : output + value
+
+      var changes = { input, output }
+
+      return change({ data, changes })
+    },
+
+    negate() {
+      var { output } = data
+
+      if (!Math.abs(output)) {
+        return
+      }
+
+      var value = output *= -1
+
+      var changes = {
+        output: value.toString()
+      }
+
+      return change({ data, changes })
+    },
+
+    percent() {
+      // https://devblogs.microsoft.com/oldnewthing/20080110-00/?p=23853
+      // 100 + 5% = 105
+      // 9 + 9% => 9 + 0.81 (9% of 9) = 9.81
+      // Apply percent to current entry, then multiply that by the previous entry,
+      // and replace the current entry with that answer.
+      // So now we need to use the input array...
+
+
+    },
+
+    reciprocal() {
+      var { output } = data
+
+      if (Number(output) < 0) {
+        // Notify state of errors
+        return state.transition({
+          error: `Cannot divide by zero`
+        })
+      }
+
+      var value = (1 / output)
+
+      var changes = {
+        output: value.toString()
+      }
+
+      return change({ data, changes })
+    },
+
+    square() {
+      var { output } = data
+
+      var value = output * output
+
+      var changes = {
+        output: value.toString()
+      }
+
+      return change({ data, changes })
+    },
+
+    squareroot() {
+      var { output } = data
+
+      if (Number(output) < 0) {
+        // Notify state of errors
+        return state.transition({
+          error: `invalid input for square root, "${output}"`
+        })
+      }
+
+      var value = Math.sqrt(output)
+
+      var changes = {
+        output: value.toString()
+      }
+
+      return change({ data, changes })
+    }
   }
 
   var model = {
     propose({ action, value }) {
-      var data = steps[action]({ value })
+      if (!(action in steps)) {
+        // send error to state
+        return state.transition({
+          error: `invalid action step, "${action}"`
+        })
+      }
 
-      // Demonstrate the dependency on state.
-      state.change({ data })
+      var changes = steps[action]({ value })
+
+      if (changes !== data) {
+        // skip transition if data not updated
+        return
+      }
+
+      state.transition({ data: Object.assign({}, changes) })
     }
   }
 
--- content/demos/calculator/test/model.js
+++ content/demos/calculator/test/model.js
@@ -4,7 +4,307 @@ import { define } from "/js/lib/sam/define.js"
 describe("model", () => {
   var { expect } = chai
 
-  it("runs", () => {
-    expect(expect).to.be.a("function")
+  describe("define({ model })", () => {
+    var app = define({ model })
+
+    it("returns app with model and state", () => {
+      var { model, state } = app;
+
+      expect(state).to.be.an("object")
+      expect(model).to.be.an("object")
+    })
+  })
+
+  describe("steps", () => {
+    var app = define({ model })
+
+    it("sends error to state on invalid action step", () => {
+      var { model, state } = app;
+
+      state.transition = function ({ error }) {
+        expect(error).to.equal(`invalid action step, "bonk"`)
+      }
+
+      model.propose({ action: "bonk" })
+    })
+
+    describe("clear", () => {
+      it("clears the model", () => {
+        var { model, state } = app;
+
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          expect(output).to.be.an("string")
+          expect(output).to.equal("0")
+          expect(input).to.be.an("array")
+          expect(input.length).to.equal(0)
+        }
+
+        model.propose({ action: "clear" })
+      })
+    })
+
+    describe("digit", () => {
+      var { model, state } = app;
+
+      beforeEach(() => {
+        state.transition = function ({ data }) { }
+
+        model.propose({ action: "clear" })
+      })
+
+      it("pushes digit to the input and output", () => {
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          expect(input.length).to.equal(1)
+          expect(output).to.equal("123")
+        }
+
+        model.propose({ action: "digit", value: "123" })
+      })
+
+      it("sends error to state if value is not a digit", () => {
+        state.transition = function ({ error }) {
+          expect(error).to.equal(`invalid digit value, "6y6"`)
+        }
+
+        model.propose({ action: "digit", value: "6y6" })
+      })
+    })
+
+    describe("clearentry", () => {
+      var { model, state } = app;
+
+      beforeEach(() => {
+        state.transition = function ({ data }) { }
+
+        model.propose({ action: "clear" })
+        model.propose({ action: "digit", value: "123" })
+      })
+
+      it("clears current entry ", () => {
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          expect(output).to.equal("0")
+        }
+
+        model.propose({ action: "clearentry" })
+      })
+    })
+
+    describe("backspace", () => {
+      var { model, state } = app;
+
+      beforeEach(() => {
+        state.transition = function ({ data }) { }
+
+        model.propose({ action: "clear" })
+        model.propose({ action: "digit", value: "123" })
+      })
+
+      it("removes last character", () => {
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          expect(output).to.equal("12")
+        }
+
+        model.propose({ action: "backspace" })
+      })
+    })
+
+    describe("decimal", () => {
+      var { model, state } = app;
+
+      beforeEach(() => {
+        state.transition = function ({ data }) { }
+
+        model.propose({ action: "clear" })
+      })
+
+      it("pushes decimal separator", () => {
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          expect(output).to.equal("0.")
+        }
+
+        model.propose({ action: "decimal" })
+      })
+
+      it("ignores entry if decimal already present", () => {
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          expect(output).to.equal("0.")
+        }
+
+        model.propose({ action: "decimal" })
+        model.propose({ action: "decimal" })
+        model.propose({ action: "decimal" })
+      })
+    })
+
+    describe("negate", () => {
+      var { model, state } = app;
+
+      beforeEach(() => {
+        state.transition = function ({ data }) { }
+
+        model.propose({ action: "clear" })
+        model.propose({ action: "digit", value: "123" })
+      })
+
+      it("negates current entry", () => {
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          expect(output).to.equal("-123")
+        }
+
+        model.propose({ action: "negate" })
+      })
+
+      it("negates negative entry to positive", () => {
+        var entry;
+
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          entry = output
+        }
+
+        model.propose({ action: "negate" })
+        model.propose({ action: "negate" })
+
+        expect(entry).to.equal("123")
+      })
+    })
+
+    describe("percent", () => {
+      // https://devblogs.microsoft.com/oldnewthing/20080110-00/?p=23853
+      it("pending")
+    })
+
+    describe("reciprocal", () => {
+      var { model, state } = app;
+
+      beforeEach(() => {
+        state.transition = function ({ data }) { }
+
+        model.propose({ action: "clear" })
+      })
+
+      it("returns the reciprocal of current entry", () => {
+        var result
+
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          result = output
+        }
+
+        model.propose({ action: "digit", value: "4" })
+        model.propose({ action: "reciprocal" })
+
+        expect(result).to.equal("0.25")
+      })
+
+      it("returns error if current entry is less than zero", () => {
+        var result
+
+        state.transition = function ({ data = {}, error = "" }) {
+          result = error
+        }
+
+        model.propose({ action: "digit", value: "4" })
+        model.propose({ action: "negate" })
+        model.propose({ action: "reciprocal" })
+
+        expect(result).to.equal("Cannot divide by zero")
+      })
+    })
+
+    describe("square", () => {
+      var { model, state } = app;
+
+      beforeEach(() => {
+        state.transition = function ({ data }) { }
+
+        model.propose({ action: "clear" })
+      })
+
+      it("returns the square of current entry", () => {
+        var result
+
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          result = output
+        }
+
+        model.propose({ action: "digit", value: "9" })
+        model.propose({ action: "square" })
+
+        expect(result).to.equal("81")
+      })
+    })
+
+    describe("squareroot", () => {
+      var { model, state } = app;
+
+      beforeEach(() => {
+        state.transition = function ({ data }) { }
+
+        model.propose({ action: "clear" })
+        model.propose({ action: "digit", value: "9" })
+      })
+
+      it("returns square root of current entry", () => {
+        state.transition = function ({ data }) {
+          var { output, input } = data
+
+          expect(output).to.equal("3")
+        }
+
+        model.propose({ action: "squareroot" })
+      })
+
+      it("returns error if current entry is less than zero", () => {
+        var entry, message
+
+        state.transition = function ({ data = {}, error = {} }) {
+          entry = data.output
+          message = error
+        }
+
+        model.propose({ action: "clear" })
+        model.propose({ action: "digit", value: "9" })
+        model.propose({ action: "negate" })
+
+        model.propose({ action: "squareroot" })
+
+        expect(message).to.equal(`invalid input for square root, "-9"`)
+      })
+    })
+
+    describe("nextOp", () => {
+
+      describe("divide", () => {
+        it("pending")
+      })
+      describe("minus", () => {
+        it("pending")
+      })
+      describe("multiply", () => {
+        it("pending")
+      })
+      describe("plus", () => {
+        it("pending")
+      })
+    })
   })
 })
